cd86361b758dd61c7ca6f6f8dbf07f859ac26020
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-04-16 17:05:11 +0400

    -

--- components/300-networking/data_sources.tf
+++ components/300-networking/data_sources.tf
@@ -5,17 +5,9 @@ data "doppler_secrets" "prod_main" {}
 data "oci_core_private_ips" "tool_server" {
   ip_address = local.networking.ip_address.tool_server
   subnet_id  = oci_core_subnet.private_mgmt.id
-
-  depends_on = [
-    oci_core_subnet.private_mgmt
-  ]
 }
 
 data "oci_core_private_ips" "app_server" {
   ip_address = local.networking.ip_address.app_server
   subnet_id  = oci_core_subnet.private_mgmt.id
-
-  depends_on = [
-    oci_core_subnet.private_mgmt
-  ]
 }
