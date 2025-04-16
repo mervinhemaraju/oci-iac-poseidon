d281613cc2e8a2e831d130c879909402193fbe17
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-04-16 17:07:47 +0400

    - Fixed subnet issue

--- components/300-networking/data_sources.tf
+++ components/300-networking/data_sources.tf
@@ -4,10 +4,18 @@ data "doppler_secrets" "prod_main" {}
 # Get ths private ips for computes
 data "oci_core_private_ips" "tool_server" {
   ip_address = local.networking.ip_address.tool_server
-  subnet_id  = oci_core_subnet.private_mgmt.id
+  subnet_id  = oci_core_subnet.public_mgmt.id
+
+  depends_on = [
+    oci_core_subnet.public_mgmt
+  ]
 }
 
 data "oci_core_private_ips" "app_server" {
   ip_address = local.networking.ip_address.app_server
-  subnet_id  = oci_core_subnet.private_mgmt.id
+  subnet_id  = oci_core_subnet.public_mgmt.id
+
+  depends_on = [
+    oci_core_subnet.public_mgmt
+  ]
 }
