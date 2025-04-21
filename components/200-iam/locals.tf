2e9339db72ce0ef0a101a833fa9e6f702327593a
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-04-21 18:39:49 +0400

    - Added iam component - Updated route table for private mgmt

--- components/200-iam/locals.tf
+++ components/200-iam/locals.tf
@@ -8,7 +8,7 @@ locals {
       "Terraform"   = "Yes",
       "Project"     = "https://github.com/mervinhemaraju/oci-iac-poseidon",
       "Environment" = "Production"
-      "Component"   = "300-networking"
+      "Component"   = "200-iam"
     }
   }
 
@@ -17,18 +17,10 @@ locals {
       production = data.doppler_secrets.prod_main.map.OCI_POSEIDON_COMPARTMENT_PRODUCTION_ID
       root       = data.doppler_secrets.prod_main.map.OCI_POSEIDON_COMPARTMENT_ROOT_ID
     }
-  }
 
-  networking = {
-
-    cidr = {
-      vcn = {
-        mgmt = "10.15.0.0/16"
-      }
-      subnets = {
-        private_mgmt = "10.15.10.0/24"
-        private_k8   = "10.15.20.0/24"
-      }
+    groups = {
+      administrators = "Administrators"
     }
   }
+
 }
--- components/300-networking/data_sources.tf
+++ components/300-networking/data_sources.tf
@@ -2,20 +2,10 @@
 data "doppler_secrets" "prod_main" {}
 
 # Get ths private ips for computes
-# data "oci_core_private_ips" "tool_server" {
-#   ip_address = local.networking.ip_address.tool_server
-#   subnet_id  = oci_core_subnet.public_mgmt.id
+data "oci_core_private_ips" "k8s" {
+  subnet_id = oci_core_subnet.private_k8.id
 
-#   depends_on = [
-#     oci_core_subnet.public_mgmt
-#   ]
-# }
-
-# data "oci_core_private_ips" "app_server" {
-#   ip_address = local.networking.ip_address.app_server
-#   subnet_id  = oci_core_subnet.public_mgmt.id
-
-#   depends_on = [
-#     oci_core_subnet.public_mgmt
-#   ]
-# }
+  depends_on = [
+    oci_core_subnet.private_k8
+  ]
+}
