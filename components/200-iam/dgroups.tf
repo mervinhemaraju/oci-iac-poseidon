9e830e596c510396c6763aba7cf11e36d45580e5
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-04-25 23:08:54 +0400

    - Updated dynamic group rule

--- components/200-iam/dgroups.tf
+++ components/200-iam/dgroups.tf
@@ -4,7 +4,7 @@ resource "oci_identity_dynamic_group" "instance_principal" {
   name           = local.values.groups.dynamic.instance_principal
 
   description   = "Dynamic group for instance principal"
-  matching_rule = "ALL {resource.type = 'instance'}"
+  matching_rule = "ANY { instance.compartment.id = '${local.values.compartments.production}' }"
 
   freeform_tags = local.tags.defaults
 }
