26863ffce9d90a633a704bf304758a565d9ca0fc
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-05-30 18:33:21 +0400

    - Removed unused data source

--- components/300-networking/data_sources.tf
+++ components/300-networking/data_sources.tf
@@ -1,11 +1,2 @@
 # Define our data source to fetch secrets
 data "doppler_secrets" "prod_main" {}
-
-# Get ths private ips for computes
-data "oci_core_private_ips" "tool_nodes" {
-  subnet_id = oci_core_subnet.private_tool.id
-
-  depends_on = [
-    oci_core_subnet.private_tool
-  ]
-}
