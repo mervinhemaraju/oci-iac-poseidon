ea8b718797d9de7cc363e6a14499f26882f53449
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-06-23 20:09:45 +0400

    - Fixed compute section outputs

--- components/400-compute/outputs.tf
+++ components/400-compute/outputs.tf
@@ -11,7 +11,7 @@ output "oke_cluster_kubernetes_version" {
 
 # The OKE public endpoint
 output "oke_cluster_public_endpoint" {
-  value = oci_containerengine_cluster.apps.endpoint_config[0].public_endpoint
+  value = oci_containerengine_cluster.apps.endpoints[0].public_endpoint
 }
 
 # The Node Pool ID
@@ -24,8 +24,13 @@ output "oke_node_pool_kubernetes_version" {
   value = oci_containerengine_node_pool.apps.kubernetes_version
 }
 
-# Nodes IDs
-output "oke_node_pool_node_ids" {
-  value = oci_containerengine_node_pool.apps.nodes[*].id
+# Nodes IDs, Private IPs, and Public IPs in the OKE Node Pool
+output "oke_node_pool_nodes" {
+  value = {
+    for node in oci_containerengine_node_pool.apps.nodes : node.id => {
+      id         = node.id
+      private_ip = node.private_ip
+      public_ip  = node.public_ip
+    }
+  }
 }
-
