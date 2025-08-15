419b83ca0bbffc5e075374c54f93ca79257dc1ed
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-15 18:23:34 +0400

    - remvoe unused outputs

--- components/600-buckets/outputs.tf
+++ components/600-buckets/outputs.tf
@@ -4,8 +4,3 @@
 output "bucket_namespace_certificates" {
   value = data.oci_objectstorage_namespace.this.namespace
 }
-
-# The certificates bucket ID
-output "bucket_id_certificates" {
-  value = oci_objectstorage_bucket.certificates.bucket_id
-}
