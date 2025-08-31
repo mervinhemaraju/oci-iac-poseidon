ba15f11337311832f66f4e127365de3e873e4bdd
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-31 14:00:08 +0400

    - Added drg attachment to vcn

--- components/300-networking/vcn.tf
+++ components/300-networking/vcn.tf
@@ -1,4 +1,4 @@
-# Create a virtual cloud network for Mgmt
+# The virtual cloud network for Mgmt and its attachments
 resource "oci_core_vcn" "mgmt" {
 
   compartment_id = local.values.compartments.production
@@ -12,4 +12,20 @@ resource "oci_core_vcn" "mgmt" {
   is_ipv6enabled = false
 
   freeform_tags = local.tags.defaults
+}
+
+# > The DRG attachment
+resource "oci_core_drg_attachment" "k8" {
+
+  drg_id = oci_core_drg.k8.id
+
+  display_name = "k8-drg-attachment"
+
+  freeform_tags = local.tags.defaults
+
+  network_details {
+    id             = oci_core_vcn.mgmt.id
+    type           = "VCN"
+    vcn_route_type = "SUBNET_CIDRS"
+  }
 }
