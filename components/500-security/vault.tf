6c3c0a5521d08113412f41faea001edda0cba0d1
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-10-07 20:04:31 +0400

    - Removed unused vaults

--- components/500-security/vault.tf
+++ components/500-security/vault.tf
@@ -1,28 +1,28 @@
-# Create a vault for uptimekuma application secrets
-module "vault_application_uptimekuma" {
-  source = "./modules/vault"
+# # Create a vault for uptimekuma application secrets
+# module "vault_application_uptimekuma" {
+#   source = "./modules/vault"
 
-  compartment_id = local.values.compartments.production
-  vault_name     = "application-uptimekuma"
+#   compartment_id = local.values.compartments.production
+#   vault_name     = "application-uptimekuma"
 
-  secrets = {
-    admin_password = base64encode(data.doppler_secrets.apps_creds.map.UPTIMEKUMA_ADMIN_PASSWORD),
-  }
+#   secrets = {
+#     admin_password = base64encode(data.doppler_secrets.apps_creds.map.UPTIMEKUMA_ADMIN_PASSWORD),
+#   }
 
-  tags = local.tags.defaults
-}
+#   tags = local.tags.defaults
+# }
 
 
-# Create a vault for cloudflare secrets
-module "vault_application_cloudflare" {
-  source = "./modules/vault"
+# # Create a vault for cloudflare secrets
+# module "vault_application_cloudflare" {
+#   source = "./modules/vault"
 
-  compartment_id = local.values.compartments.production
-  vault_name     = "application-cloudflare"
+#   compartment_id = local.values.compartments.production
+#   vault_name     = "application-cloudflare"
 
-  secrets = {
-    admin_password = base64encode(data.doppler_secrets.apps_creds.map.CLOUDFLARE_TERRAFORM_TOKEN),
-  }
+#   secrets = {
+#     admin_password = base64encode(data.doppler_secrets.apps_creds.map.CLOUDFLARE_TERRAFORM_TOKEN),
+#   }
 
-  tags = local.tags.defaults
-}
+#   tags = local.tags.defaults
+# }
