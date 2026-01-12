877f56f40c1a5666b7a4d03bca9dd502243faa82
Mervin Hemaraju <th3pl4gu33@gmail.com>
2026-01-12 22:46:48 +0400

    - Fixed RPC for zeus

--- components/300-networking/rpc.tf
+++ components/300-networking/rpc.tf
@@ -15,7 +15,7 @@ resource "oci_core_remote_peering_connection" "zeus_prod" {
   drg_id         = oci_core_drg.k8.id
 
   display_name     = "rpc-zeus-prod"
-  peer_region_name = "af-johannesburg-1" # ZEUS region
+  peer_region_name = var.region
 
   freeform_tags = local.tags.defaults
 }
